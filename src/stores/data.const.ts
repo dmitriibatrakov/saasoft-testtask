3526a4635dfcc69686f8e77944e979388574a240
DmitriiBatrakov <Batrmladshiy@gmail.com>
2026-02-04 11:24:36 +0300

    feat: added sessionStorage for temp save data (p.5 testTask)

---
+++ src/stores/data.const.ts
@@ -0,0 +1,32 @@
+import type { DataType } from "@/types/types";
+
+export const fakeData: DataType[] = [
+    {
+      id: 1,
+      tag: [{ text: 'xxx' }, { text: 'yyyy' }, { text: 'kkkkkkkk' }, { text: 'MMMMMM' }],
+      type: 1,
+      login: 'so22meLogin',
+      password: null,
+    },
+    {
+      id: 2,
+      tag: [{ text: 'xxx' }, { text: 'yyyy' }, { text: 'kkkkkkkk' }, { text: 'MMMMMM' }],
+      type: 2,
+      login: 'so666meLogin',
+      password: 'som2ePass',
+    },
+    {
+      id: 3,
+      tag: [{ text: 'xxx' }, { text: 'yyy3224y' }, { text: 'kkkkkkkk' }, { text: 'MMMMMM' }],
+      type: 1,
+      login: 'som111eLogin',
+      password: null,
+    },
+    {
+      id: 4,
+      tag: [{ text: 'xxx' }, { text: 'yy123y' }, { text: 'kkkkkkkk' }, { text: 'MMMMMM' }],
+      type: 2,
+      login: 'som5555eLogin',
+      password: 'so5mePass',
+    },
+  ]
--- src/stores/data.store.ts
+++ src/stores/data.store.ts
@@ -1,38 +1,14 @@
 import type { AccountTypeType, DataType } from '@/types/types';
 import { defineStore } from 'pinia';
-import { computed, ref } from 'vue';
+import { computed, ref, watch } from 'vue';
+import { fakeData } from './data.const';
 
 export const useDataStore = defineStore('fakeData', () => {
-  const data = ref<DataType[]>([
-    {
-      id: 1,
-      tag: [{ text: 'xxx' }, { text: 'yyyy' }, { text: 'kkkkkkkk' }, { text: 'MMMMMM' }],
-      type: 1,
-      login: 'so22meLogin',
-      password: null,
-    },
-    {
-      id: 2,
-      tag: [{ text: 'xxx' }, { text: 'yyyy' }, { text: 'kkkkkkkk' }, { text: 'MMMMMM' }],
-      type: 2,
-      login: 'so666meLogin',
-      password: 'som2ePass',
-    },
-    {
-      id: 3,
-      tag: [{ text: 'xxx' }, { text: 'yyy3224y' }, { text: 'kkkkkkkk' }, { text: 'MMMMMM' }],
-      type: 1,
-      login: 'som111eLogin',
-      password: null,
-    },
-    {
-      id: 4,
-      tag: [{ text: 'xxx' }, { text: 'yy123y' }, { text: 'kkkkkkkk' }, { text: 'MMMMMM' }],
-      type: 2,
-      login: 'som5555eLogin',
-      password: 'so5mePass',
-    },
-  ]);
+  const data = ref<DataType[]>(JSON.parse(sessionStorage.getItem('SSFakeData') ?? JSON.stringify(fakeData)));
+
+  watch(data, (newData) => {
+    sessionStorage.setItem('SSFakeData', JSON.stringify(newData))
+  }, { immediate: true, deep: true })
 
   const renderData = computed(() => {
     return data.value.map((item) => {
